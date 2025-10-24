2f1dc96cdc49cf68ec0c54237fd5a07ee920a90c
Mike <shelliha@s21-edu.ru>
2025-10-24 13:02:51 +0300

    add all Standart Operations

---
+++ src/StandartOperations/DivOperation.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "IOperation.h"
+
+class Div : public IOperation
+{
+
+public:
+    std::string getName() const override {
+        return "/";
+    }
+    
+    size_t getArgumentCount() const override {
+        return 2;
+    }
+    
+    float execute(const std::vector<float>& args) override {
+        if (args.size() != getArgumentCount()) {
+            // throw std::invalid_argument("Plus requires exactly 2 argument");
+        }
+        return args[0] / args[1];
+    }
+    
+};
