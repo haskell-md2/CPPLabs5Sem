09ef8af0f3a5f29f03b81313ebf491add3dd4735
Mike <shelliha@s21-edu.ru>
2025-10-30 15:38:55 +0300

    add exemples for custom plugins

---
+++ src/plugins/DegOperation.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include "../Interfaces/PluginInterface.h"
+
+class DegOperation : public IOperation {
+public:
+    std::string getName() const override {
+        return "^";
+    }
+    
+    size_t getArgumentCount() const override {
+        return 2;
+    }
+    
+    int getPriority() const override {
+        return 3;
+    }
+    
+    float execute(const std::vector<float>& args) override {
+        return std::pow(args[0], args[1]);
+    }
+};
+
+extern "C" {
+    IOperation* createOperation() {
+        return new DegOperation();
+    }
+    
+    void destroyOperation(IOperation* operation) {
+        delete operation;
+    }
+}
---
+++ src/plugins/SinOperation.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include "../Interfaces/PluginInterface.h"
+
+class SinOeration : public IOperation {
+public:
+    std::string getName() const override {
+        return "sin";
+    }
+    
+    size_t getArgumentCount() const override {
+        return 1;
+    }
+    
+    int getPriority() const override {
+        return 4;
+    }
+    
+    float execute(const std::vector<float>& args) override {
+        return std::sin(args[0]);
+    }
+};
+
+extern "C" {
+    IOperation* createOperation() {
+        return new SinOeration();
+    }
+    
+    void destroyOperation(IOperation* operation) {
+        delete operation;
+    }
+}
